3e188fc2168168aac31cb9532e618a96aae0d3f5
lpreiss-wsei <63652555+lpreiss-wsei@users.noreply.github.com>
2021-04-08 01:32:30 +0200

    Update index.ts

--- lab3/index.ts
+++ lab3/index.ts
@@ -11,14 +11,14 @@ class App {
     }
 
     initMenu(): void {
-        const menuContainer = <HTMLDivElement>(document.createElement('div')); // kontener menu dostŕpnych gier
-        const gameContainer = <HTMLDivElement>(document.createElement('div')); // kontener g│ˇwny ekranu z gr╣
-        const list = document.createElement('ul'); // lista pozycji w menu dostŕpnych gier
+        const menuContainer = <HTMLDivElement>(document.createElement('div')); // kontener menu dostępnych gier
+        const gameContainer = <HTMLDivElement>(document.createElement('div')); // kontener główny ekranu z grą
+        const list = document.createElement('ul'); // lista pozycji w menu dostępnych gier
         
-        // TODO: Zaimplementuj wzorzec fabryki/metody fabrykuj╣cej, tak aby na podstawie konkretnej wartoťci z enum
-        // zwrˇciŠ obiekt gry. Z tego obiektu mo┐na nastŕpnie pobraŠ nazwŕ gry i dodaŠ do menu oraz metodŕ zwracaj╣c╣
-        // sam╣ grŕ i po klikniŕciu w wybrany element listy wywo│ywaŠ j╣, aby doklejaŠ zawartoťŠ do gameContainer.
-        // Aby wyťwietliŠ menu nale┐y napisaŠ pŕtlŕ, ktˇra przeiteruje po wszystkich wartoťciach enum'a
+        // TODO: Zaimplementuj wzorzec fabryki/metody fabrykującej, tak aby na podstawie konkretnej wartości z enum
+        // zwrócić obiekt gry. Z tego obiektu można następnie pobrać nazwę gry i dodać do menu oraz metodę zwracającą
+        // samą grę i po kliknięciu w wybrany element listy wywoływać ją, aby doklejać zawartość do gameContainer.
+        // Aby wyświetlić menu należy napisać pętlę, któta przeiteruje po wszystkich wartoťciach enum'a
 
         menuContainer.appendChild(list);
         document.body.appendChild(menuContainer);

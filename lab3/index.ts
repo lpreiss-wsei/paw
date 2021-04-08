d57ee8b7983ac2b0a33b064e4382a09d6820ef3f
lpreiss-wsei <63652555+lpreiss-wsei@users.noreply.github.com>
2021-04-08 11:58:23 +0200

    Update index.ts

--- lab3/index.ts
+++ lab3/index.ts
@@ -7,10 +7,10 @@ import './styles/styles.scss';
 class App {
 
     constructor() {
-        this.initMenu();        
+        this.init();        
     }
 
-    initMenu(): void {
+    init(): void {
         const menuContainer = <HTMLDivElement>(document.createElement('div')); // kontener menu dostępnych gier
         const gameContainer = <HTMLDivElement>(document.createElement('div')); // kontener główny ekranu z grą
         const list = document.createElement('ul'); // lista pozycji w menu dostępnych gier
@@ -18,7 +18,7 @@ class App {
         // TODO: Zaimplementuj wzorzec fabryki/metody fabrykującej, tak aby na podstawie konkretnej wartości z enum
         // zwrócić obiekt gry. Z tego obiektu można następnie pobrać nazwę gry i dodać do menu oraz metodę zwracającą
         // samą grę i po kliknięciu w wybrany element listy wywoływać ją, aby doklejać zawartość do gameContainer.
-        // Aby wyświetlić menu należy napisać pętlę, któta przeiteruje po wszystkich wartoťciach enum'a
+        // Aby wyświetlić menu należy napisać pętlę, któta przeiteruje po wszystkich wartościach enum'a
 
         menuContainer.appendChild(list);
         document.body.appendChild(menuContainer);
@@ -26,4 +26,4 @@ class App {
     }
 }
 
-new App(gameFactory);
+new App();
